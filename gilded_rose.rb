c085f66c382ec9bd2d1699ce974a82661b8f0989
Francho Joven <lab@francho.org>
2016-06-06 20:09:15 +0200

    Style fixup: remove unnecessary double quotes

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -27,7 +27,7 @@ def update_quality(items)
       item.sell_in -= 1
     end
     if item.sell_in < 0
-      if item.name != "Aged Brie"
+      if item.name != 'Aged Brie'
         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
           if item.quality > 0
             if item.name != 'Sulfuras, Hand of Ragnaros'
