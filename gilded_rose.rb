0857be97095950d0b5ced3d8d8716da02d57cf6a
Logan Hope Koch <loganville@gmail.com+>
2017-04-13 11:04:16 -0600

    kata session 1

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,48 +1,76 @@
+def update_quality_concert_pass(item)
+  if item.sell_in < 6
+    increase_quality(item, 3)
+  elsif item.sell_in < 11
+    increase_quality(item, 2)
+  else
+    increase_quality(item)
+  end
+end
+
+def decrease_quality(item)
+  if item.quality > 0
+    if item.name != 'Sulfuras, Hand of Ragnaros'
+      item.quality -= 1
+    end
+  end
+end
+
+def decrease_sell_in(item)
+  if item.name != 'Sulfuras, Hand of Ragnaros'
+    item.sell_in -= 1
+  end
+end
+
+def quality_is_not_maxed(item)
+  item.quality < 50
+end
+
+def increase_quality(item, amount = 1)
+  if quality_is_not_maxed(item)
+    item.quality += amount
+  end
+end
+
+def item_expired(item)
+  item.sell_in < 0
+end
+
+def is_brie(item)
+  item.name == 'Aged Brie'
+end
+
+def is_concert_pass(item)
+  item.name == 'Backstage passes to a TAFKAL80ETC concert'
+end
+
+def set_to_worthless(item)
+  item.quality = 0
+end
+
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
+
+    if is_brie(item)
+      increase_quality(item)
+    elsif is_concert_pass(item)
+      update_quality_concert_pass(item)
     else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
+      decrease_quality(item)
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != 'Aged Brie'
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
+
+    decrease_sell_in(item)
+
+    if item_expired(item)
+      if is_brie(item)
+        increase_quality(item)
+      elsif is_concert_pass(item)
+        set_to_worthless(item)
       else
-        if item.quality < 50
-          item.quality += 1
-        end
+        decrease_quality(item)
       end
     end
+
   end
 end
 
